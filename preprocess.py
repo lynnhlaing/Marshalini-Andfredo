5cd511fe75f9efcb82815b103c966b290b75b413
Marshall Vyletel <marshallvyletel@marshalls-mbp.devices.brown.edu>
2019-12-08 16:50:05 -0500

    Update to preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,18 @@
-#aaafdsfsadfasfasf
+import hdf5_getters as hdf
+import numpy as np
+import tensorflow as tf
+import matplotlib as plot
+
+
+def read_songs(filename):
+	songs_file = hdf.open_h5_file_read(filename)
+
+	print("Start time of Fade out = ", hdf.get_start_of_fade_out(songs_file))
+
+
+def main():
+	read_songs("./MillionSongSubset/data/A/A/A/TRAAAVG12903CFA543.h5")
+
+
+if __name__ == '__main__':
+   main()
