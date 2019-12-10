8e3b870a3687c313a603539080f117ae84d7fa5d
Marshall Vyletel <marshallvyletel@marshalls-mbp.devices.brown.edu>
2019-12-09 20:51:56 -0500

    Added network and set up preprocess write to file

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-import preprocess.py
+import preprocess
 import tensorflow as tf
 from tensorflow.keras import Model
 
@@ -9,50 +9,130 @@ class Inception_Model(tf.keras.Model):
     def __init__(self):
         '''Init hyperparameters and layers'''
         #hyperparameters
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-        self.batch_size = 100
-        self.inception_filters = 10
-        #layers
-        self.incpetion_1 = tf.keras.layers.Conv2D(filters=self.inception_filters, kernel_size=[12,4], padding='same',activation='relu')
-        self.incpetion_2 = tf.keras.layers.Conv2D(filters=self.inception_filters, kernel_size=[14,8], padding='same',activation='relu')
-        self.incpetion_3 = tf.keras.layers.Conv2D(filters=self.inception_filters, kernel_size=[16,16], padding='same',activation='relu')
 
-        self.concatenate = tf.keras.layers.concatenate()
+        self.batch_size = 128
+        self.inception_filters = 36
+        #layers
+        self.inception_1 = tf.keras.layers.Conv2D(filters=self.inception_filters, kernel_size=[12,4], padding='same',activation='relu')
+        self.inception_2 = tf.keras.layers.Conv2D(filters=self.inception_filters, kernel_size=[14,8], padding='same',activation='relu')
+        self.inception_3 = tf.keras.layers.Conv2D(filters=self.inception_filters, kernel_size=[16,16], padding='same',activation='relu')
+
+        self.concatenate = tf.keras.layers.concatenate(axis=3)
 
         self.max_pool1 = tf.keras.layers.MaxPool2D(pool_size=2)
-
-        self.conv_1 = tf.keras.layers.Conv2D(filters=self.inception_filters, kernel_size=[1,4], padding='valid',activation='relu')
-
         self.max_pool2 = tf.keras.layers.MaxPool2D(pool_size=2)
-        self.max_pool3 = tf.keras.layers.MaxPool2D(pool_size=2)
+
+        self.conv_1 = tf.keras.layers.Conv2D(filters=self.inception_filters, kernel_size=[1,4], padding='same',activation='relu')
+
+        self.max_pool3 = tf.keras.layers.MaxPool2D(pool_size=(2,3))
+        self.max_pool4 = tf.keras.layers.MaxPool2D(pool_size=(2,3))
 
         self.conv_2 = tf.keras.layers.Conv2D(filters=self.inception_filters/2, kernel_size=[1,1], padding='same',activation='relu')
         self.conv_3 = tf.keras.layers.Conv2D(filters=self.inception_filters/4, kernel_size=[1,1], padding='same',activation='relu')
-        self.conv_4 = tf.keras.layers.Conv2D(filters=1, kernel_size=[1,80], padding='same',activation='softmax')
+        self.conv_4 = tf.keras.layers.Conv2D(filters=1, kernel_size=[1,1], padding='same',actvation="softmax")
 
+        #self.dense = tf.keras.layers.Dense(units = 2)
+
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
 
     @tf.function
     def call(self, inputs):
-        '''model call'''
-        pass
+        '''runs the model architecture and outputs the classification logits
+        input = MFCC encoded timbre data for 1024 segments of each song each having 12 features (batch_size,1024,12)
+        output = dancibility prediction value
+        '''
+        inception1 = self.inception_1(inputs)
+        inception2 = self.inception_2(inputs)
+        inception3 = self.inception_3(inputs)
+        combined = self.concatenate((inception1,inception2,inception3))
+
+        max1 = self.max_pool2(self.max_pool1(combined))
+
+        conv1 = self.conv_1(max1)
+
+        max2 = self.max_pool4(self.max_pool3(conv1))
+
+        out = self.conv_4(self.conv_3(self.conv_2(max2)))
+
+        #return tf.dense(out)
+        return reduce_mean(out)
+
+    def create_classes(self, probs):
+        return tf.greater(probs,0.5)
 
     @tf.function
-    def loss_function(self, inputs, labels);
-        pass
+    def loss_function(self, probs, labels):
+        return tf.reduce_mean(tf.keras.losses.binary_crossentropy(labels,self.create_classes(probs)))
 
-    def accuracy_function(self, output, labels):
-        pass
+    def accuracy_function(self, probs, labels):
+        assignments = self.create_classes(probs)
+        correct = 0
+        acc = 0
+        for song in assignments:
+            if song == labels[acc]:
+                correct+=1
+            acc+=1
+        return correct/assignments.shape[0]
+
+
+def create_kfolds(data, labels, folds=10):
+    assert(data.shape(0) == labels.shape(0))
+    #7400 --> 10 x 740 7400, 1024 x 12
+    data = data[0:7400]
+    labels = labels[0:7400]
+    batched_data = np.reshape(data, (10,740,1024,12))
+    batched_labels = np.reshape(labels, (10,740,1))
+
+    return batched_data, batched_labels
+
 
 
 def train(model, train_inputs, train_labels):
-    pass
+    num_batches = (train_inputs.shape)[0]
+    for i in range(num_batches):
+        batch_inputs = np.squeeze(train_inputs[i,:,:],axis=0)
+        batch_labels = np.squeeze(train_labels[i,:],axis=0)
+        with tf.GradientTape() as tape:
+            probs = model.call(batch_inputs)
+            loss = model.loss_function(probs,batch_labels)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        model.optimizer.apply_gradients(zip(gradients,model.trainable_variables))
+
+
 
 def test(model, test_inputs, test_labels):
-    pass
+    probs = model.call(test_inputs)
+    accuracy = model.accuracy_function(probs,test_labels)
+    print(accuracy)
 
 def main():
-    pass
+    data, labels = preprocess.get_data("./MillionSongSubset/")
+    # model = Inception_Model()
+    # num_epochs = 5
+    # while i <= num_epochs:
+    #     num_songs = data.shape[0]
+    # 	idx = np.arange(num_songs)
+    # 	shuffled = tf.random.shuffle(idx)
+    # 	shuffled_inputs = tf.gather(data, shuffled, axis=0)
+    # 	shuffled_labels = tf.gather(labels, shuffled, axis=0)
+    #     batched_data, batched_labels = create_kfolds(data, labels)
+    #     for i in range(0, batched_data.shape(0)):
+    #         if i == 0:
+    #             train_data = batched_data[1:batched_data.shape(0)]
+    #             train_labels = batched_labels[1:batched_data.shape(0)]
+    #             test_data = batched_data[0,:,:,:]
+    #             test_labels = batched_labels[0,:,:]
+    #         else:
+    #             train_data = np.stack((batched_data[:i],batched_data[i:]),axis=0)
+    #             train_labels = np.stack((batched_labels[:i],batched_labels[i:]),axis=0)
+    #             test_data = batched_data[i]
+    #             test_labels = batched_labels[i]
+    #
+    #         train(model,train_data,train_labels)
+    #         test(model,test_data,test_labels)
+
+
 
 if __name__ == '__main__':
     main()
--- preprocess.py
+++ preprocess.py
@@ -11,11 +11,15 @@ import spotipy.util as util
 import spotipy.oauth2 as oauth2
 import re
 
+DATA_SIZE = 1024
+
 def get_all_files(basedir,ext) :
     """
     From a root directory, go through all subdirectories
     and find all files with the given extension.
-    Return all absolute paths in a list.
+
+    param: base directory from which to begin the search, extension to find
+    returns: a list of all full path names to each file with ext as extension
     """
     allfiles = []
     for root, dirs, files in os.walk(basedir):
@@ -23,6 +27,123 @@ def get_all_files(basedir,ext) :
         for f in files :
             allfiles.append( os.path.abspath(f) )
     return allfiles
+
+def get_danceability(query, sp):
+    """
+    Makes a query to spotify to get the danceability for each song in our
+    dataset
+
+    param: the query to search Spotify, the spotify object to make calls to
+    returns: 1 if the song is danceable, 0 if not
+    """
+    search = sp.search(query,type='track', limit =5)
+    if search["tracks"]["items"]:
+        trackID = search["tracks"]["items"][0]["uri"]
+        features = sp.audio_features(tracks=[trackID])
+        danceability = features[0]["danceability"]
+        if danceability >= 0.6:
+            dance_track = 1
+        else:
+            dance_track = 0
+    else:
+        dance_track = -1
+    return dance_track
+
+def create_data(songs, labels):
+    """
+    Goes through all songs in list to find the MFCC timbre data to train on
+
+    param: the list of song absolute file path names, and the danceability
+    labels for each
+    returns: a [num_songs, 1024, 12] array of data, and a modified labels list
+    with only valid songs
+    """
+    print("creating data...")
+    unique, counts = np.unique(labels, return_counts=True)
+    num_occurences = dict(zip(unique, counts))
+    num_valid_songs = num_occurences[1] + num_occurences[0]
+    data = np.zeros([num_valid_songs, DATA_SIZE, 12])
+    new_labels = []
+    acc = 0
+    for i in range(1,len(songs)-1):
+        if labels[i] != -1:
+            new_labels.append(labels[i])
+            song_file = hdf.open_h5_file_read(songs[i])
+            MFCC_data = np.array(hdf.get_segments_timbre(song_file))
+            if MFCC_data.shape[0] < DATA_SIZE:
+                pad_amount = DATA_SIZE - MFCC_data.shape[0]
+                pad_array = np.zeros([pad_amount, 12])
+                MFCC_data = np.vstack((MFCC_data, pad_array))
+            elif MFCC_data.shape[0] > DATA_SIZE:
+                MFCC_data = MFCC_data[0:DATA_SIZE, :]
+            data[acc] = MFCC_data
+            acc+=1
+            song_file.close()
+    new_labels = np.array(new_labels)
+    return data, labels
+
+def create_labels(songs, sp):
+    # Songs now holds list of all file paths to each song as a string
+    """
+    Goes through all songs in list to find the danceability from spotify
+
+    param: the list of song absolute file path names, and the Spotify object to
+    use to make calls
+    returns: a [num_songs] array of danceability labels (-1,0,1)
+    """
+    print("creating labels...")
+    acc = 0
+    labels = np.zeros([len(songs) -1])
+    for i in range(1,len(songs)):
+        file_object= hdf.open_h5_file_read(songs[i])
+        artist_name = hdf.get_artist_name(file_object).decode("utf-8")
+        title = re.sub(r"\(.*\)","",hdf.get_title(file_object).decode("utf-8"))
+        query = "artist: " + artist_name + " track: " + title
+        label = get_danceability(query, sp)
+        if label == -1:
+            acc+=1
+        labels[i-1] = label
+        file_object.close()
+        print(i)
+    print("NUMBER OF LOST SONGS = ", acc)
+    return labels
+
+def get_audio_features(songID, sp):
+    """
+    Returns a json object containing the audio features of a given song
+
+    param: the unique spotify TrackID, the spotify object
+    returns: JSON object of features
+    """
+    return sp.audio_features(tracks=[songID])
+
+def get_data(filename):
+    """
+    Creates the data and labels for all valid songs in the dataset
+
+    param: the root file name with all song data
+    returns: data: [num_valid_songs, 1024, 12] matrix of MFCC content,
+        labels: [num_valid_songs] array of danceability label
+    """
+    credentials = oauth2.SpotifyClientCredentials(
+        client_id='b8aaedbcc185427d96c4f92151d23951',
+        client_secret='8be53030941e409cb0e7c6196ad979fd')
+    token = credentials.get_access_token()
+    sp = spotipy.Spotify(auth=token)
+    songs = get_all_files(filename, '.h5')
+    primary_labels = create_labels(songs, sp)
+    np.savetxt('test.txt', primary_labels, delimiter=',')
+    data,labels = create_data(songs, primary_labels)
+    print("SIZE OF DATA = ", data.shape)
+    print("SIZE OF LABELS = ", labels.shape)
+
+    return data, labels
+
+# if __name__ == '__main__':
+#    get_data("./MillionSongSubset/")
+
+''' ----------------------------------------------------------------------------
+Code to load JSON data that holds Spotify Track ID for each song in dataset if it exists
 
 def get_spotify_json(basedir, songID, ext) :
     """
@@ -37,56 +158,11 @@ def get_spotify_json(basedir, songID, ext) :
             allfiles.append( os.path.abspath(f) )
     return allfiles
 
-def get_danceability(query, sp):
-    search = sp.search(query,type='track', limit =5)
-    if search["tracks"]["items"]:
-        track = search["tracks"]["items"][0]["uri"]
-        features = sp.audio_features(tracks=[track])
-        danceability = features[0]["danceability"]
-        if (danceability>=0.6):
-            dance_track = 1
-        else:
-            dance_track = 0
-    else:
-        dance_track = -1
-
-    return dance_track
-
-def read_songs(filename,sp):
-    songs = get_all_files(filename, '.h5')
-    labels = []
-    for i in range(1,len(songs)):
-        file_object= hdf.open_h5_file_read(songs[i])
-        echo_ID = hdf.get_song_id(file_object)
-        echo_ID = echo_ID.decode("utf-8")
-        json_file_name = get_spotify_json("./millionsongdataset_echonest/", echo_ID, ".json")[0]
-        with open(json_file_name, 'r') as myfile:
-            data=myfile.read()
-        obj = json.loads(data)
-        title = re.sub(r"\(.*\)","",hdf.get_title(file_object).decode("utf-8"))
-        # print(title)
-        query = "artist: " + hdf.get_artist_name(file_object).decode("utf-8") + " track: " + title
-        label = get_danceability(query, sp)
-        labels.append(label)
-        file_object.close()
-    print(labels)
-
-def get_audio_features(songID):
-    credentials = oauth2.SpotifyClientCredentials(
-        client_id='b8aaedbcc185427d96c4f92151d23951',
-        client_secret='8be53030941e409cb0e7c6196ad979fd')
-    token = credentials.get_access_token()
-
-    return sp.audio_features(tracks=[songID])
-
-def main():
-    credentials = oauth2.SpotifyClientCredentials(
-        client_id='b8aaedbcc185427d96c4f92151d23951',
-        client_secret='8be53030941e409cb0e7c6196ad979fd')
-    token = credentials.get_access_token()
-    sp = spotipy.Spotify(auth=token)
-    read_songs("./MillionSongSubset/", sp)
-
-
-if __name__ == '__main__':
-   main()
+echo_ID = hdf.get_song_id(file_object)
+echo_ID = echo_ID.decode("utf-8")
+json_file_name = get_spotify_json("./millionsongdataset_echonest/", echo_ID, ".json")[0]
+with open(json_file_name, 'r') as myfile:
+  data=myfile.read()
+obj = json.loads(data)
+  ----------------------------------------------------------------------------
+'''
